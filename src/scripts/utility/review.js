f8efc957f3010d64032beeb3e01faa16fe96811e
jimmy <jimmy>
2021-05-24 11:58:25 +0700

    back up 24 may 2021 111:58 am

--- src/scripts/utility/review.js
+++ src/scripts/utility/review.js
@@ -1,5 +1,17 @@
 // import { restaurantDetailAPI } from '../data/api';
 
+const reviewCommentFilter = (Comment) => {
+  let status = true;
+  console.log(Comment);
+  const forbidentString = /(div)/gi;
+  console.log(Comment.match(forbidentString));
+  if (Comment.match(forbidentString)) {
+    status = false;
+  }
+
+  return status;
+};
+
 const reviewInitiator = {
   async init({ riviewElemenet, restaurantDetailData }) {
     this.riviewElemenet = riviewElemenet;
@@ -9,29 +21,34 @@ const reviewInitiator = {
   },
 
   createReviewRow(riviewData) {
+    let returnelement = '';
     const {
       date, name, review,
     } = riviewData;
 
-    return `
-      <review-row>
-        <name>
-          ${name}
-        </name>
-        <review>
-          ${review}
-        </review>
-        <date>
-          ${date}
-        </date>
-      </review-row>
-    `;
+    if (reviewCommentFilter(review)) {
+      returnelement = `
+        <review-row>
+          <name>
+            ${name}
+          </name>
+          <review>
+            ${review}
+          </review>
+          <date>
+            ${date}
+          </date>
+        </review-row>
+      `;
+    }
+
+    return returnelement;
   },
 
   async render() {
-    this.restaurantDetailData.customerReviews.forEach((riviewIndex, riviewData) => {
+    this.restaurantDetailData.customerReviews.forEach((riviewData, riviewIndex) => {
       const element = this.createReviewRow(riviewData);
-      if (riviewIndex > 0) {
+      if (riviewIndex === 0) {
         this.riviewElemenet.innerHTML = element;
       } else {
         this.riviewElemenet.innerHTML += element;
